820fec08efce2f53b8837f7c63d60493c7c10b36
Gerard Paapu <gerard@gerardpaapu.com>
2022-12-03 11:55:58 +1300

    feat: finished day 1 in js
    also rewrote it in a weird fp style, because I guess
    thats the mindset Im in now

--- 001/solution.js
+++ 001/solution.js
@@ -1,24 +1,60 @@
+#!/usr/bin/env node
 const main = async () => {
   const data = await require('node:fs/promises').readFile(
     './input.txt',
     'utf-8'
   )
-  const lines = data.split('\n')
-  const elves = []
+
+  let elves = Id(data)
+    .map(lines)
+    .map(splitBy(eq('')))
+    .map(
+      mapWith(
+        mapWith((x) => Number(x)),
+        sum
+      )
+    ).value
+
+  const step1 = maximum(elves)
+  const step2 = sum(elves.sort(desc).slice(0, 3))
+
+  console.log({ step1, step2 })
+}
+
+const Id = (value) => ({ map: (f) => Id(f(value)), value })
+
+const eq = (a) => (b) => a === b
+const sum = (a) => a.reduce((a, b) => a + b, 0)
+const maximum = (a) => Math.max(...a)
+const lines = (s) => s.split('\n')
+const desc = (a, b) => b - a
+
+const mapWith =
+  (...fs) =>
+  (a) =>
+    fs.reduce((a, f) => a.map(f), a)
+
+const findIndex =
+  (f, start = 0) =>
+  (arr) => {
+    for (let i = start; i < arr.length; i++) {
+      if (f(arr[i])) return i
+    }
+
+    return -1
+  }
+
+const splitBy = (f) => (arr) => {
+  const result = []
   let start = 0
   let end
-  while ((end = lines.indexOf('', start)) !== -1) {
-    elves.push(lines.slice(start, end))
+  while ((end = findIndex(f, start)(arr)) !== -1) {
+    result.push(arr.slice(start, end))
     start = end + 1
   }
 
-  elves.push(lines.slice(start))
-
-  let e1 = elves.map((elf) =>
-    elf.map((n) => Number(n)).reduce((a, b) => a + b, 0)
-  )
-
-  console.log(Math.max(...e1))
+  result.push(arr.slice(start))
+  return result
 }
 
 main().catch((e) => {
